3b4d3d2fffbd694762cae7b00ba05e4bf8422cdf
Md_Owais's-Repo <ansariowais669@gmail.com>
2026-08-18 23:50:21 +0530

    Time: 1 ms (72.24%), Space: 17.6 MB (85.9%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+
+        vector<vector<pair<int, int>>> graph(n);
+
+        // u -> {v, weight}
+        for(int i = 0; i < flights.size(); i++) {
+            int u = flights[i][0];
+            int v = flights[i][1];
+            int wt = flights[i][2];   // Correct weight
+
+            graph[u].push_back({v, wt});
+        }
+
+        // {node, {cost, stops}}
+        queue<pair<int, pair<int, int>>> q;
+
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        // src with 0 cost and 0 stops
+        q.push({src, {0, 0}});
+
+        while(!q.empty()) {
+
+            auto curr = q.front();
+            q.pop();
+
+            int u = curr.first;
+            int cost = curr.second.first;
+            int stops = curr.second.second;
+
+            // We can use at most k stops => k + 1 flights
+            if(stops == k + 1)
+                continue;
+
+            for(auto edge : graph[u]) {
+
+                int v = edge.first;
+                int wt = edge.second;
+
+                if(cost + wt < dist[v]) {
+
+                    dist[v] = cost + wt;
+
+                    q.push({
+                        v,
+                        {dist[v], stops + 1}
+                    });
+                }
+            }
+        }
+
+        if(dist[dst] == INT_MAX)
+            return -1;
+
+        return dist[dst];
+    }
+};
